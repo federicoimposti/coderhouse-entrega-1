0d210bd2a661221cb1e8532126d72210c5677c5a
mase <federico@kumelen.studioclear>
2021-01-02 15:36:35 -0300

    verificar git

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprendiendo React
+          Aprendiendo React js
         </a>
       </header>
     </div>
