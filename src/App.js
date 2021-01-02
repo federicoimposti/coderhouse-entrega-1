6cf8522cf04d476d1e9758ab6844a6577c5e2530
mase <federico@kumelen.studioclear>
2021-01-02 15:38:24 -0300

    Update year react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bienvenido 2020
+          Bienvenido 2021
         </p>
         <a
           className="App-link"
